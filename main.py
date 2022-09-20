be491ef0a01c557d4083c1ac92d011545c945009
Ramirez-Facu <98377628+Ramirez-Facu@users.noreply.github.com>
2022-09-20 19:47:41 -0300

    Add fun: skip_if_add / checks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+from tkinter import E
 import selenium.webdriver as webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,10 +8,31 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
- 
-## agregue este comentario
-
 import time
+
+def skip_if_add(driver):
+    try:
+        wait = WebDriverWait(driver, 20*60)
+        element = wait.until(EC.element_to_be_clickable(driver.find_element(By.CLASS_NAME, "ytp-ad-skip-button-container")))
+        element.click()
+        print("1")
+    except:
+        
+        pass
+
+def check_vid(driver):
+    Element_Content= driver.find_elements(By.ID, "content")[0]
+    Element_Content.click()
+
+def check_login(driver):
+    driver.find_element(By.XPATH, '//*[@id="buttons"]/ytd-button-renderer').click()
+    driver.implicitly_wait(5)
+    time.sleep(2)
+    driver.back()
+
+def check_volume_mute(driver):
+    Element_mute=driver.find_element(By.CLASS_NAME, 'ytp-volume-area')
+    Element_mute.click()
 
 def launchBrowser():
     edge_options = Options()
@@ -21,60 +43,18 @@ def launchBrowser():
     time.sleep(10)
     
     driver.implicitly_wait(5)
-
-    Element_loggin= driver.find_element(By.XPATH, '//*[@id="buttons"]/ytd-button-renderer/a')
-    Element_loggin.click()
-    
-    driver.implicitly_wait(5)
-
-    time.sleep(5)
-    driver.back()
+    check_login(driver)
     time.sleep(5)
     driver.implicitly_wait(5)
-    Element_Content= driver.find_elements(By.ID, "content")[0]
-
-    Element_Content.click()
-    
-    try:
-        Element_add=WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="skip-button:5"]/span/button')))# WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id="skip-button:p"]/span/button"))
-        Element_add.clic()
-        print("aca")
-    except:
-        print("no llego")
-        pass
-
-   
-    try:
-        Element_add2=WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="skip-button:5"]/span/button')))# WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id="skip-button:p"]/span/button"))
-        Element_add2.clic()
-        print("si llego")
-    except:
-        pass
-    
-    Element_mute = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.XPATH, '//*[@id="movie_player"]/div[34]/div[2]/div[1]/span/button'))
-    Element_mute.click()
-    time.sleep(5)
+    check_vid(driver)
+    skip_if_add(driver)
+    skip_if_add(driver)
+    driver.implicitly_wait(5)   
+    check_volume_mute(driver)
     time.sleep(15)
     driver.back()
-
+    driver.close()
     while(True):
         pass
     
-    
-def closeBrowser():
-    pass
-
 launchBrowser()
-
-# def launchBrowser():
-#     #user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/105.0.0.0'
-# # os.environ['PATH'] += os.pathsep + "C:/SeleniumDrivers"
-#     edge_driver_path= os.path.join(os.getcwd(), 'msedgedriver.exe')
-#     edge_service = Service(edge_driver_path)
-#     edge_options= Options()
-#     #edge_options.add_argument(f'user-agent={user_agent}')
-#     browser = webdriver.Edge(service= edge_service, options=edge_options)
-#     edge_options.add_experimental_option("detach", True)
-#     browser.get("http://www.google.com/")
-#     return browser
-# Edge= launchBrowser()
